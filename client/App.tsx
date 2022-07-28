ccfedaedbea7c79654c2574b69df6d4494ba1861
Andreas Asséhn <andreas.assehn@gmail.com>
2022-07-28 16:10:01 +0200

    Starting React Native

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Andreas!</Text>
       <StatusBar style="auto" />
     </View>
   );
